dd27e9d817043c3e502ba9178d4a16132ea0c03c
Katie Leonard <kleonard@newrelic.com>
2016-10-24 13:16:31 -0700

    Add readme.

---
+++ README.md
@@ -0,0 +1,8 @@
+## Titlecase
+
+// split the string into array
+// uppercase the first char
+// join the words
+// exclude things like a an the (unless it is the first word)
+
+An npm package that adds a `titlecase` function to the String prototype.
--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// split the string into array
-// uppercase the first char
-// join the words
-// exclude things like a an the (unless it is the first word)
 String.prototype.titlecase = function () {
   const wordArray = this.split(' ')
   const newString = wordArray.map(word => {
