d937b373635bd31250df3494c2a52d64198089fe
Katie Leonard <kleonard@newrelic.com>
2016-10-24 10:29:51 -0700

    Add test harness.

---
+++ test/test_titlecase.js
@@ -0,0 +1,11 @@
+import chai from 'chai'
+
+const titlecase = require('../index.js');
+const should = chai.should();
+const expect = chai.expect;
+
+describe('titlecase', () => {
+  it('should pass this canary test', () => {
+    expect(true).to.be.true
+  })
+})
